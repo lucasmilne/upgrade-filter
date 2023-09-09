f8bbc0b503efb1fdce49b76c67c1f58fa90f7d17
Lucas Milne <32175567+lucasmilne@users.noreply.github.com>
2023-09-09 13:44:21 -0700

    update badge

--- src/main/java/com/lockeddoors/upgradefilter/UpgradeFilter.java
+++ src/main/java/com/lockeddoors/upgradefilter/UpgradeFilter.java
@@ -1,15 +1,18 @@
 package com.lockeddoors.upgradefilter;
 
 import basemod.BaseMod;
+import basemod.ModPanel;
 import basemod.interfaces.EditStringsSubscriber;
+import basemod.interfaces.PostInitializeSubscriber;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
 import com.megacrit.cardcrawl.localization.UIStrings;
 
 import java.net.URL;
 
 @SpireInitializer
-public class UpgradeFilter implements EditStringsSubscriber {
+public class UpgradeFilter implements EditStringsSubscriber, PostInitializeSubscriber {
     private static final String modID = "upgradefilter";
 
     public static void initialize() {
@@ -46,8 +49,19 @@ public class UpgradeFilter implements EditStringsSubscriber {
         BaseMod.loadCustomStringsFile(UIStrings.class, path);
     }
 
+    private static String getImageLocation(String image) {
+        String path = UpgradeFilter.class.getPackage().getName().replace(".", "/");
+        return path + "/img/" + image;
+    }
+
     private String getUIStringsLocation(String language) {
         String path = this.getClass().getPackage().getName().replace(".", "/");
         return path + "/localization/" + language + "/UI-Strings.json";
+    }
+
+    @Override
+    public void receivePostInitialize() {
+        ModPanel settingsPanel = new ModPanel();
+        BaseMod.registerModBadge(ImageMaster.loadImage(getImageLocation("badge.png")), modID, "keymaster", ">Allows deck screen filtering by upgrade", settingsPanel);
     }
 }
